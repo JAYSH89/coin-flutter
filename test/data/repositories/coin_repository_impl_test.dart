ad58267dce0069478d9a43714675726823a1efd6
Jay <sanjaysheombar@gmail.com>
2024-01-26 10:07:08 +0100

    updated getAllCoins tests

--- test/data/repositories/coin_repository_impl_test.dart
+++ test/data/repositories/coin_repository_impl_test.dart
@@ -1,29 +1,22 @@
+
 import 'dart:io';
+import 'dart:convert';
 
 import 'package:coin_flutter/core/errors/server_exception.dart';
+import 'package:coin_flutter/data/models/coins/coin_dto.dart';
 import 'package:coin_flutter/data/models/trending/trending_response_dto.dart';
 import 'package:coin_flutter/data/repositories/coin_repository_impl.dart';
+import 'package:coin_flutter/domain/models/coins/coin.dart';
 import 'package:coin_flutter/domain/models/trending/trending.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import '../../helpers/json_reader.dart';
 import '../../helpers/test_helper.mocks.dart';
 
 void main() {
   late MockCoinRemoteDataSource datasource;
   late CoinRepositoryImpl coinRepositoryImpl;
-
-  const testTrending = Trending(
-    coins: [],
-    nfts: [],
-    categories: [],
-  );
-
-  const testTrendingDTO = TrendingResponseDTO(
-    coins: [],
-    nfts: [],
-    categories: [],
-  );
 
   setUp(() {
     datasource = MockCoinRemoteDataSource();
@@ -31,6 +24,18 @@ void main() {
   });
 
   group('test coin repository searchTrending', () {
+    const testTrending = Trending(
+      coins: [],
+      nfts: [],
+      categories: [],
+    );
+
+    const testTrendingDTO = TrendingResponseDTO(
+      coins: [],
+      nfts: [],
+      categories: [],
+    );
+
     test('should return trending when call datasource successful', () async {
       // arrange
       when(datasource.getTrendingCoins())
@@ -68,6 +73,57 @@ void main() {
         await coinRepositoryImpl.getTrendingCoins();
       }, throwsA(isA<SocketException>()));
       verify(datasource.getTrendingCoins());
+      verifyNoMoreInteractions(datasource);
+    });
+  });
+
+  group('test coin repository getAllCoins', () {
+    test('should return List<Coin> when call datasource successful', () async {
+      // arrange
+      final jsonFile = readJson('helpers/dummy_data/dummy_coins_markets.json');
+      final List<dynamic> jsonList = json.decode(jsonFile);
+      final data = jsonList
+          .map((e) => CoinDTO.fromJson(e as Map<String, dynamic>))
+          .toList();
+
+      when(datasource.getAllCoins())
+          .thenAnswer((realInvocation) async => data);
+
+      // act
+      final result = await coinRepositoryImpl.getAllCoins();
+
+      // assert
+      expect(result, isA<List<Coin>>());
+      expect(result.length, equals(100));
+
+      verify(datasource.getAllCoins());
+      verifyNoMoreInteractions(datasource);
+    });
+
+    test('should server failure if call datasource unsuccessful', () {
+      // arrange
+      when(datasource.getAllCoins()).thenThrow(ServerException());
+
+      // act + assert
+      expect(() async {
+        await coinRepositoryImpl.getAllCoins();
+      }, throwsA(isA<ServerException>()));
+
+      verify(datasource.getAllCoins());
+      verifyNoMoreInteractions(datasource);
+    });
+
+    test('should connection failure if no internet connection', () {
+      // arrange
+      when(datasource.getAllCoins())
+          .thenThrow(const SocketException('Failed to connect to the network'));
+
+      // act + assert
+      expect(() async {
+        await coinRepositoryImpl.getAllCoins();
+      }, throwsA(isA<SocketException>()));
+
+      verify(datasource.getAllCoins());
       verifyNoMoreInteractions(datasource);
     });
   });
